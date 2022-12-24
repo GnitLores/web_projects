1a6d465c6733d2a8ee0ea20433e6cdbb1a4f1e4e
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-24 12:36:45 +0100

    part 5 done

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const app = Vue.createApp({
             url: 'https://www.google.com',
             inventory: 118,
             onSale: true,
+            details: ["50% cotton", "30% wool", "20% polyester"],
+            variants: [
+                { id: 2234, color: "green" },
+                { id: 2235, color: "blue" },
+            ],
         }
     }
 
