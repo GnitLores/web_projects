d807c4966f8bcc40f07062bf2377e348b96773a8
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-25 17:02:26 +0100

    part 8 done

--- main.js
+++ main.js
@@ -3,34 +3,63 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            brand: "SuperSocks",
+            selectedVariant: 0,
             description: 'Got a lot of sole due to a hole',
             url: 'https://www.google.com',
-            inventory: 4,
-            onSale: true,
             details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
-                { id: 2234, color: "green", image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: "blue", image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: "green", image: './assets/images/socks_green.jpg', quantity: 4 , onSale: true},
+                { id: 2235, color: "blue", image: './assets/images/socks_blue.jpg', quantity: 0 , onSale: false},
             ],
         }
     },
     methods: {
         addToCart() {
-            if (this.inventory > 0) {
+            if (this.inStock) {
             this.cart += 1
-            this.inventory -= 1
         }
         },
         removeFromCart() {
-            if (this.cart > 0) {
+            if (!this.cartEmpty) {
                 this.cart -= 1
-                this.inventory += 1
             }
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            // console.log(index)
         },
+    },
+    computed: {
+        title() {
+            str = this.brand + " " + this.product
+            if (this.onSale) {
+                str = str + " is on sale!"
+            }
+            return str
+        },
+        variant() {
+            return this.variants[this.selectedVariant]
+        },
+        quantity() {
+            return this.variant.quantity - this.cart
+        },
+        inStock() {
+            return this.quantity > 0
+        },
+        lowStock() {
+            return this.quantity > 0 && this.quantity <= 3 
+        },
+        image() {
+            return this.variant.image
+        },
+        onSale() {
+            return this.variant.onSale
+        },
+        cartEmpty() {
+            return this.cart == 0
+        },
+        
     },
 
 })
