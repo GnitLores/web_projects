7e76c475081c7020973a10c4c1639f2d3dd1e22a
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-27 11:25:34 +0100

    style buttons

--- vue_firebase/src/App.vue
+++ vue_firebase/src/App.vue
@@ -2,13 +2,27 @@
   <div class="todo">
     <div class="title has-text-centered">Todo</div>
 
-    <div class="field is-grouped">
+    <div class="field is-grouped mb-5">
       <p class="control is-expanded">
         <input class="input" type="text" placeholder="Add a todo" />
       </p>
       <p class="control">
         <a class="button is-info"> Add </a>
       </p>
+    </div>
+
+    <div v-for="i in 3" class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">Brew coffee</div>
+            <div class="column is-6 has-text-right">
+              <button class="button is-light">&check;</button>
+              <button class="button is-danger ml-2">&cross;</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
