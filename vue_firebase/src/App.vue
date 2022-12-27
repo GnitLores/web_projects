c24bdbed3bd5f567d29f2e14f9e259f2b4b5e871
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-27 11:12:16 +0100

    add form button

--- vue_firebase/src/App.vue
+++ vue_firebase/src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  Todo
+  <div class="todo">
+    <div class="title has-text-centered">Todo</div>
+
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="Add a todo" />
+      </p>
+      <p class="control">
+        <a class="button is-info"> Add </a>
+      </p>
+    </div>
+  </div>
 </template>
 
 <style>
-@import 'bulma/css/bulma.min.css';
+@import "bulma/css/bulma.min.css";
+
+.todo {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
 </style>
