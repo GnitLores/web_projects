5b28908d6bf4e4b444ebf27982b2ea6880970c52
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-27 13:43:26 +0100

    add firebase

---
+++ vue_firebase/src/firebase/index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmRFyqKaGp7icx4HHnm699LFnlTL7CWcg",
+
+  authDomain: "todo-a85eb.firebaseapp.com",
+
+  projectId: "todo-a85eb",
+
+  storageBucket: "todo-a85eb.appspot.com",
+
+  messagingSenderId: "184755466476",
+
+  appId: "1:184755466476:web:b2f9d114369c32c987f5c9",
+};
+
+const app = initializeApp(firebaseConfig);
