0a3eb23672cd0abff2a32d555753c40f6618612a
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-27 12:43:50 +0100

    generate ids

--- vue_firebase/src/App.vue
+++ vue_firebase/src/App.vue
@@ -2,14 +2,23 @@
   <div class="todo">
     <div class="title has-text-centered">Todo</div>
 
-    <div class="field is-grouped mb-5">
-      <p class="control is-expanded">
-        <input class="input" type="text" placeholder="Add a todo" />
-      </p>
-      <p class="control">
-        <a class="button is-info"> Add </a>
-      </p>
-    </div>
+    <form @submit.prevent="addTodo">
+      <div class="field is-grouped mb-5">
+        <p class="control is-expanded">
+          <input
+            v-model="newTodoContent"
+            class="input"
+            type="text"
+            placeholder="Add a todo"
+          />
+        </p>
+        <p class="control">
+          <button :disabled="!newTodoContent" class="button is-info">
+            Add
+          </button>
+        </p>
+      </div>
+    </form>
 
     <div v-for="todo in todos" class="card mb-5">
       <div class="card-content">
@@ -29,19 +38,31 @@
 
 <script setup>
 import { ref } from "vue";
+import { v4 as uuidv4 } from "uuid";
 
 const todos = ref([
-  {
-    id: "id1",
-    content: "brew coffee",
-    done: false,
-  },
-  {
-    id: "id2",
-    content: "drink coffee",
-    done: false,
-  },
+  // {
+  //   id: "id1",
+  //   content: "brew coffee",
+  //   done: false,
+  // },
+  // {
+  //   id: "id2",
+  //   content: "drink coffee",
+  //   done: false,
+  // },
 ]);
+
+const newTodoContent = ref("");
+
+const addTodo = () => {
+  const newTodo = {
+    id: uuidv4(),
+    content: newTodoContent.value,
+    done: false,
+  };
+  console.log("newTodo: ", newTodo);
+};
 </script>
 
 <style>
