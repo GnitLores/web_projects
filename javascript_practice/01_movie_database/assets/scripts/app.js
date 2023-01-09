26a95886bf8ecb68d3f0829689356e93fe877030
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-09 09:14:09 +0100

    add movies

--- javascript_practice/01_movie_database/assets/scripts/app.js
+++ javascript_practice/01_movie_database/assets/scripts/app.js
@@ -4,8 +4,32 @@ const backdrop = document.getElementById("backdrop");
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
+const entryTextSection = document.getElementById("entry-text");
 
 const movies = [];
+
+const updateUi = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+  <div class="movie-element__image">
+  <img src="${imageUrl}" alt="${title}" />
+  </div>
+  <div class="movie-element__info">
+  <h2>${rating}/5 stars</h2>
+  </div>;
+  `;
+  const listRoot = document.getElementById("movie-list");
+  listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -50,6 +74,8 @@ const addMovieHandler = () => {
   console.log(movies);
   toggleMovieModal();
   clearMovieInput();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUi();
 };
 
 const backdropClickHandler = () => {
