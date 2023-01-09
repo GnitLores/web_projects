72029846f18dbc4db985ff846ae0b4c1e076e2a3
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-09 08:41:55 +0100

    clear inputs

--- javascript_practice/01_movie_database/assets/scripts/app.js
+++ javascript_practice/01_movie_database/assets/scripts/app.js
@@ -5,6 +5,8 @@ const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
 
+const movies = [];
+
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
 };
@@ -13,8 +15,15 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
+const clearMovieInput = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = "";
+  }
+};
+
 const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
 };
 
 const addMovieHandler = () => {
@@ -31,6 +40,16 @@ const addMovieHandler = () => {
   ) {
     alert("Please enter valid values (rating between 1 and 5)");
   }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
 };
 
 const backdropClickHandler = () => {
