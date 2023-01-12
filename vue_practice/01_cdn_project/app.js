65db2b4bf62b2ffa08679dbe4829132274e7d121
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-12 10:49:54 +0100

    short hand event directives

--- vue_practice/01_cdn_project/app.js
+++ vue_practice/01_cdn_project/app.js
@@ -5,11 +5,15 @@ const vm = Vue.createApp({
       lastName: "Doe",
       url: "https://google.com",
       raw_url: '<p><a href="https://google.com" target="_blank">Google</a></p>',
+      age: 20,
     };
   },
   methods: {
     fullName() {
       return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+    increment() {
+      this.age++;
     },
   },
 }).mount("#app");
