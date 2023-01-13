237046bd977d76abb9f44f58c798dfd583adc0ad
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-13 19:03:31 +0100

    css transitions

--- vue_practice/08_animations_project/src/App.vue
+++ vue_practice/08_animations_project/src/App.vue
@@ -1,11 +1,35 @@
 <template>
-  <div></div>
+  <div>
+    <button type="button" @click="flag = !flag">Toggle</button>
+
+    <transition name="fade">
+      <h2 v-if="flag">Hello world!</h2>
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      flag: false,
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  // values: property time function
+  transition: all 0.25s linear;
+}
+// Default style is opacity 1, so no need to apply
+.fade-leave-to {
+  transition: all 0.25s linear;
+  opacity: 0;
+}
+</style>
