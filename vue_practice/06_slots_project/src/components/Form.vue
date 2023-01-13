d20186701e93ff29c89c4fa2fe68949ec7414fcf
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-13 17:21:46 +0100

    using named slots to modify component

--- vue_practice/06_slots_project/src/components/Form.vue
+++ vue_practice/06_slots_project/src/components/Form.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <form><slot>No form to render</slot></form>
+    <form>
+      <div class="help"><slot name="help"></slot></div>
+      <div class="fields"><slot name="fields"></slot></div>
+      <div class="buttons"><slot name="buttons"></slot></div>
+    </form>
+    <slot></slot>
   </div>
 </template>
 
