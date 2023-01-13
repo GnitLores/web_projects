4f0054998c9a1af52cac4cd4e36385a4378081f0
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-13 17:37:48 +0100

    dynamic components

---
+++ vue_practice/07_dynamic_components/src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // Lifecycle hooks:
+  unmounted() {
+    // Will NOT get called if keep-alive element is used
+    console.log('About component unmounted');
+  },
+  activated() {
+    // WILL get called if keep-alive element is used
+    console.log('About component activated');
+  },
+  deactivated() {
+    // WILL get called if keep-alive element is used
+    console.log('About component deactivated');
+  },
+};
+</script>
---
+++ vue_practice/07_dynamic_components/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // Lifecycle hooks:
+  unmounted() {
+    // Will NOT get called if keep-alive element is used
+    console.log('Home component unmounted');
+  },
+  activated() {
+    // WILL get called if keep-alive element is used
+    console.log('Home component activated');
+  },
+  deactivated() {
+    // WILL get called if keep-alive element is used
+    console.log('Home component deactivated');
+  },
+};
+</script>
