a168483930df8faf8d1f25fb974d9e96d7e5b5b4
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-13 17:09:36 +0100

    modifying child component with slots

--- vue_practice/06_slots_project/src/App.vue
+++ vue_practice/06_slots_project/src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <!-- Alternative syntax for using component -->
-  <app-form />
+  <app-form>
+    <div class="help"><p>This is some help text.</p></div>
+    <div class="fields">
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+    </div>
+    <div class="buttons">
+      <button type="submit">Submit</button>
+    </div>
+  </app-form>
+  <app-form></app-form>
 </template>
 
 <script>
--- vue_practice/06_slots_project/src/components/Form.vue
+++ vue_practice/06_slots_project/src/components/Form.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
-    <form>
-      <div class="help"><p>This is some help text.</p></div>
-      <div class="fields">
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-      </div>
-      <div class="buttons">
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <form><slot>No form to render</slot></form>
   </div>
 </template>
 
