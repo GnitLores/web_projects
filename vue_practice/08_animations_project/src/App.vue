be44afd5da5fe6600a9519455ebbb9af7b9a54f6
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-13 19:29:50 +0100

    css animations

--- vue_practice/08_animations_project/src/App.vue
+++ vue_practice/08_animations_project/src/App.vue
@@ -4,9 +4,14 @@
 
     <!-- Keys should be used with multiple elements -->
     <!-- out-in mode makes elements animate out before elements animate in -->
-    <transition name="fade" mode="out-in">
+    <!-- <transition name="fade" mode="out-in">
       <h2 v-if="flag" key="main">Hello world!</h2>
       <h2 v-else key="secondary">Another hello!</h2>
+    </transition> -->
+
+    <!-- appear makes the animation play on page load -->
+    <transition name="zoom" type="animation" appear>
+      <h2 v-if="flag">Hello</h2>
     </transition>
   </div>
 </template>
@@ -16,23 +21,60 @@ export default {
   name: 'App',
   data() {
     return {
-      flag: false,
+      flag: true,
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.fade-enter-from {
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+// .fade-enter-from {
+//   opacity: 0;
+// }
+// .fade-enter-active {
+//   // values: property time function
+//   transition: all 0.5s linear;
+// }
+// // Default style is opacity 1, so no need to apply
+// .fade-leave-to {
+//   transition: all 0.5s linear;
+//   opacity: 0;
+// }
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 1s linear;
+}
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 1s linear;
+}
+.zoom-enter-from {
   opacity: 0;
 }
-.fade-enter-active {
-  // values: property time function
-  transition: all 0.5s linear;
-}
-// Default style is opacity 1, so no need to apply
-.fade-leave-to {
-  transition: all 0.5s linear;
+.zoom-leave-to {
   opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
