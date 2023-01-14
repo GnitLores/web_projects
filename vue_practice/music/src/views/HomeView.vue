a5c345d0e0cdd568ac81ba6151dee9fc6424d80b
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-14 14:28:15 +0100

    prepare project

--- vue_practice/music/src/views/HomeView.vue
+++ vue_practice/music/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
