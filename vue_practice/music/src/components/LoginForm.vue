91cc38e7bfc63c04ad3df27081c4b28d03129aa4
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-15 12:39:00 +0100

    validate login form

--- vue_practice/music/src/components/LoginForm.vue
+++ vue_practice/music/src/components/LoginForm.vue
@@ -1,4 +1,11 @@
 <template>
+  <div
+    class="text-white text-center font-bold p-4 rounded mb-4"
+    v-if="login_show_alert"
+    :class="login_alert_variant"
+  >
+    {{ login_alert_msg }}
+  </div>
   <vee-form :validation-schema="loginSchema" @submit="login">
     <!-- Email -->
     <div class="mb-3">
@@ -24,6 +31,7 @@
     </div>
     <button
       type="submit"
+      :disabled="login_in_submission"
       class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
     >
       Submit
@@ -41,10 +49,21 @@ export default {
         email: "required|email",
         password: "required|min:3|max:100|excluded:password",
       },
+      login_in_submission: false,
+      login_show_alert: false,
+      login_alert_variant: "bg-blue-500",
+      login_alert_msg: "Please wait! We are loggin you in.",
     };
   },
   methods: {
     login(values) {
+      this.login_show_alert = true;
+      this.login_in_submission = true;
+      this.login_alert_variant = "bg-blue-500";
+      this.login_alert_msg = "Please wait! We are loggin you in.";
+
+      this.login_alert_variant = "bg-green-500";
+      this.login_alert_msg = "Success! You have been logged in.";
       console.log(values);
     },
   },
