63df9b6aee39cdc4cb569a18943554a90ec19595
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-15 10:08:48 +0100

    required rule applied using schema

--- vue_practice/music/src/components/AppAuth.vue
+++ vue_practice/music/src/components/AppAuth.vue
@@ -89,7 +89,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -99,6 +99,7 @@
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -176,6 +177,15 @@ export default {
   data() {
     return {
       tab: "login",
+      schema: {
+        name: "required",
+        email: "",
+        age: "",
+        password: "",
+        confirm_password: "",
+        country: "",
+        tos: "",
+      },
     };
   },
   computed: {
