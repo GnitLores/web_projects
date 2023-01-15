6ee2354b76c6b083006de25eb29ad6ca0c19ca58
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-15 12:33:35 +0100

    split into components

---
+++ vue_practice/music/src/components/LoginForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <vee-form :validation-schema="loginSchema" @submit="login">
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <vee-field
+        type="email"
+        name="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email"
+      />
+      <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <vee-field
+        type="password"
+        name="password"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Password"
+      />
+      <ErrorMessage class="text-red-600" name="password" />
+    </div>
+    <button
+      type="submit"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+    >
+      Submit
+    </button>
+  </vee-form>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      tab: "login",
+      loginSchema: {
+        email: "required|email",
+        password: "required|min:3|max:100|excluded:password",
+      },
+    };
+  },
+  methods: {
+    login(values) {
+      console.log(values);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
