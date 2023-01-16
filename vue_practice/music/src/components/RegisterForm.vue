e594a3d0a921cb2df1ccece4455e9a9f73982ae1
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-16 13:24:04 +0100

    use pinia action functionto register

--- vue_practice/music/src/components/RegisterForm.vue
+++ vue_practice/music/src/components/RegisterForm.vue
@@ -107,8 +107,7 @@
 </template>
 
 <script>
-import { auth, usersCollection } from "@/includes/firebase";
-import { mapWritableState } from "pinia";
+import { mapActions } from "pinia";
 import useUserStore from "@/stores/user";
 
 export default {
@@ -133,52 +132,28 @@ export default {
       reg_alert_msg: "Please wait! Your account is being created.",
     };
   },
-  computed: {
-    ...mapWritableState(useUserStore, ["userLoggedIn"]),
-  },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: "register",
+    }),
     async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_msg = "Please wait! Your account is being created.";
 
-      let userCred = null;
       try {
-        userCred = await auth.createUserWithEmailAndPassword(
-          values.email,
-          values.password
-        );
+        await this.createUser(values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = "bg-red-500";
         this.reg_alert_msg =
           "An unexpected error occured. Please try again later.";
-        console.log(error);
         return;
       }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        });
-      } catch (error) {
-        this.reg_in_submission = false;
-        this.reg_alert_variant = "bg-red-500";
-        this.reg_alert_msg =
-          "An unexpected error occured. Please try again later.";
-        console.log(error);
-        return;
-      }
-
-      this.userLoggedIn = true;
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
-      console.log(userCred);
     },
   },
 };
--- vue_practice/music/src/stores/user.js
+++ vue_practice/music/src/stores/user.js
@@ -1,7 +1,22 @@
 import { defineStore } from "pinia";
+import { auth, usersCollection } from "@/includes/firebase";
 
 export default defineStore("user", {
   state: () => ({
     userLoggedIn: false,
   }),
+  actions: {
+    async register(values) {
+      await auth.createUserWithEmailAndPassword(values.email, values.password);
+
+      await usersCollection.add({
+        name: values.name,
+        email: values.email,
+        age: values.age,
+        country: values.country,
+      });
+
+      this.userLoggedIn = true;
+    },
+  },
 });
