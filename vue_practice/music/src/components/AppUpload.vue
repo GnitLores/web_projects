1cde3e970548cd6721508b659c2f57d75e036e63
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-17 14:18:47 +0100

    fallback upload

--- vue_practice/music/src/components/AppUpload.vue
+++ vue_practice/music/src/components/AppUpload.vue
@@ -19,6 +19,7 @@
       >
         <h5>Drop your files here</h5>
       </div>
+      <input type="file" multiple @change="upload($event)" />
       <hr class="my-6" />
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
@@ -53,7 +54,10 @@ export default {
   methods: {
     upload(event) {
       this.is_dragover = false;
-      const files = [...event.dataTransfer.files];
+
+      const files = event.dataTransfer
+        ? [...event.dataTransfer.files]
+        : [...event.target.files];
 
       files.forEach((file) => {
         if (file.type !== "audio/mpeg") {
