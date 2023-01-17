9be3a526ee7a88400bcfc751e8b55f10306f9e05
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-17 14:01:39 +0100

    store file data

--- vue_practice/music/src/components/AppUpload.vue
+++ vue_practice/music/src/components/AppUpload.vue
@@ -40,7 +40,7 @@
 </template>
 
 <script>
-import { storage } from "@/includes/firebase";
+import { storage, auth, songsCollection } from "@/includes/firebase";
 
 export default {
   name: "AppUpload",
@@ -87,7 +87,19 @@ export default {
             this.uploads[uploadIndex].text_class = "text-red-400";
             console.log(error);
           },
-          () => {
+          async () => {
+            const song = {
+              uid: auth.currentUser.uid,
+              display_name: auth.currentUser.displayName,
+              original_name: task.snapshot.ref.name,
+              modified_name: task.snapshot.ref.name,
+              genre: "",
+              comment_count: 0,
+            };
+
+            song.url = await task.snapshot.ref.getDownloadURL();
+            await songsCollection.add(song);
+
             this.uploads[uploadIndex].variant = "bg-green-400";
             this.uploads[uploadIndex].icon = "fas fa-check";
             this.uploads[uploadIndex].text_class = "text-green-400";
--- vue_practice/music/src/includes/firebase.js
+++ vue_practice/music/src/includes/firebase.js
@@ -19,5 +19,6 @@ const db = firebase.firestore();
 const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
+const songsCollection = db.collection("songs");
 
-export { auth, db, usersCollection, storage };
+export { auth, db, usersCollection, songsCollection, storage };
