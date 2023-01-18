eb3a471b45b96afd36147695c051a7406737dad3
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 14:04:28 +0100

    icon directive

---
+++ vue_practice/music/src/directives/icon.js
@@ -0,0 +1,6 @@
+export default {
+  beforeMount(el) {
+    el.innerHTML +=
+      '<i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>';
+  },
+};
--- vue_practice/music/src/main.js
+++ vue_practice/music/src/main.js
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
+import Icon from "./directives/icon";
 
 import "./assets/base.css";
 import "./assets/main.css";
@@ -18,6 +19,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia());
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.directive("icon", Icon);
 
     app.mount("#app");
   }
