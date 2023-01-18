4417ceb361aab8a1e17ac708d93ab1bbea8c1fc7
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 12:36:13 +0100

    play song

--- vue_practice/music/src/stores/player.js
+++ vue_practice/music/src/stores/player.js
@@ -1,12 +1,21 @@
 import { defineStore } from "pinia";
+import { Howl } from "howler";
 
 export default defineStore("player", {
   state: () => ({
     current_song: {},
+    sound: {},
   }),
   actions: {
     async newSong(song) {
       this.current_song = song;
+
+      this.sound = new Howl({
+        src: [song.url],
+        html5: true,
+      });
+
+      this.sound.play();
     },
   },
 });
