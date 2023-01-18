c8c16af17d47f0e3d6fa14d5b36c5f827fe65b47
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 10:38:41 +0100

    use query parameter for sort selection

--- vue_practice/music/src/views/SongView.vue
+++ vue_practice/music/src/views/SongView.vue
@@ -127,6 +127,10 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+
+    this.sort = sort === "1" || sort === "2" ? sort : "1";
+
     this.song = docSnapshot.data();
     this.getComments();
   },
@@ -169,6 +173,18 @@ export default {
           ...doc.data(),
         }),
       ]);
+    },
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return;
+      }
+      this.$router.push({
+        query: {
+          sort: newVal,
+        },
+      });
     },
   },
 };
