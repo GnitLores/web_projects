9667266bf2365037edd1f47b1a76cb71c5297e4e
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 10:09:40 +0100

    complete comment form

--- vue_practice/music/src/views/SongView.vue
+++ vue_practice/music/src/views/SongView.vue
@@ -29,18 +29,33 @@
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
-        <form>
-          <textarea
+        <div
+          class="text-white text-center font-bold p-4 mb-4"
+          v-if="comment_show_alert"
+          :class="comment_alert_variant"
+        >
+          {{ comment_alert_message }}
+        </div>
+        <vee-form
+          :validation-schema="schema"
+          @submit="addComment"
+          v-if="userLoggedIn"
+        >
+          <vee-field
+            as="textarea"
+            name="comment"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
             placeholder="Your comment here..."
-          ></textarea>
+          ></vee-field>
+          <ErrorMessage class="text-red-600" name="comment" />
           <button
             type="submit"
             class="py-1.5 px-3 rounded text-white bg-green-600 block"
+            :disabled="comment_in_submission"
           >
             Submit
           </button>
-        </form>
+        </vee-form>
         <!-- Sort Comments -->
         <select
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
@@ -69,14 +84,26 @@
 </template>
 
 <script>
-import { songsCollection } from "@/includes/firebase";
+import { auth, songsCollection, commentsCollection } from "@/includes/firebase";
+import { mapState } from "pinia";
+import useUserStore from "@/stores/user";
 
 export default {
   name: "SongView",
   data() {
     return {
       song: {},
+      schema: {
+        comment: "required|min:3",
+      },
+      comment_in_submission: false,
+      comment_show_alert: false,
+      comment_alert_variant: "bg-blue-500",
+      comment_alert_message: "Please wait! Your comment is being submitted.",
     };
+  },
+  computed: {
+    ...mapState(useUserStore, ["userLoggedIn"]),
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -87,6 +114,31 @@ export default {
     }
 
     this.song = docSnapshot.data();
+  },
+  methods: {
+    async addComment(values, { resetForm }) {
+      this.comment_in_submission = true;
+      this.comment_show_alert = true;
+      this.comment_alert_variant = "bg-blue-500";
+      this.comment_alert_message =
+        "Please wait! Your comment is being submitted.";
+
+      const comment = {
+        content: values.comment,
+        datePosted: new Date().toString(),
+        sid: this.$route.params.id,
+        name: auth.currentUser.displayName,
+        uid: auth.currentUser.uid,
+      };
+
+      await commentsCollection.add(comment);
+
+      this.comment_in_submission = false;
+      this.comment_alert_variant = "bg-green-500";
+      this.comment_alert_message = "Comment added!";
+
+      resetForm();
+    },
   },
 };
 </script>
