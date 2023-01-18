27fb992a08076eaba85f3e669500145161ff0be7
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 14:10:10 +0100

    make icon directive dynamic

--- vue_practice/music/src/directives/icon.js
+++ vue_practice/music/src/directives/icon.js
@@ -1,6 +1,6 @@
 export default {
-  beforeMount(el) {
-    el.innerHTML +=
-      '<i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>';
+  beforeMount(el, binding) {
+    const iconClass = `fa fa-${binding.value} float-right text-green-400 text-2xl`;
+    el.innerHTML += `<i class="${iconClass}"></i></i>`;
   },
 };
