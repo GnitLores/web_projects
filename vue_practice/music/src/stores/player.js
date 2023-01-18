e422d3afb33c0598229cd096131e3b50f6df7601
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 13:29:24 +0100

    playback progress bar

--- vue_practice/music/src/stores/player.js
+++ vue_practice/music/src/stores/player.js
@@ -8,6 +8,7 @@ export default defineStore("player", {
     sound: {},
     seek: "00:00",
     duration: "00:00",
+    playerProgress: "0%",
   }),
   actions: {
     async newSong(song) {
@@ -36,6 +37,10 @@ export default defineStore("player", {
     progress() {
       this.seek = helper.formatTime(this.sound.seek());
       this.duration = helper.formatTime(this.sound.duration());
+
+      this.playerProgress = `${
+        (this.sound.seek() / this.sound.duration()) * 100
+      }%`;
 
       if (this.sound.playing()) {
         requestAnimationFrame(this.progress);
