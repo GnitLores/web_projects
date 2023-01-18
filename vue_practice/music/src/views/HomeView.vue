7da99cd104c8a5479581336dfe02872ca1be7e74
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 09:28:26 +0100

    infinite scrolling

--- vue_practice/music/src/views/HomeView.vue
+++ vue_practice/music/src/views/HomeView.vue
@@ -48,7 +48,11 @@ export default {
   name: "HomeView",
   components: { AppSongItem },
   data() {
-    return { songs: [] };
+    return {
+      songs: [],
+      maxPerPage: 3,
+      pendingRequest: false,
+    };
   },
   async created() {
     this.getSongs();
@@ -66,11 +70,32 @@ export default {
         Math.round(scrollTop) + innerHeight === offsetHeight;
 
       if (bottomOfWindow) {
-        console.log("Bottom of window");
+        this.getSongs();
       }
     },
     async getSongs() {
-      const snapshots = await songsCollection.get();
+      if (this.pendingRequest) {
+        return;
+      }
+      this.pendingRequest = true;
+
+      let snapshots;
+
+      if (this.songs.length) {
+        const lastDoc = await songsCollection
+          .doc(this.songs[this.songs.length - 1].docID)
+          .get();
+        snapshots = await songsCollection
+          .orderBy("modified_name")
+          .startAfter(lastDoc)
+          .limit(this.maxPerPage)
+          .get();
+      } else {
+        snapshots = await songsCollection
+          .orderBy("modified_name")
+          .limit(this.maxPerPage)
+          .get();
+      }
 
       snapshots.forEach((document) => {
         this.songs.push({
@@ -78,6 +103,8 @@ export default {
           ...document.data(),
         });
       });
+
+      this.pendingRequest = false;
     },
   },
 };
