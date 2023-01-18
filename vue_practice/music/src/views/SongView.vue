2b7dc8dac3f34596da464de1e263db562f47cda4
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 10:15:40 +0100

    show comments

--- vue_practice/music/src/views/SongView.vue
+++ vue_practice/music/src/views/SongView.vue
@@ -68,17 +68,18 @@
   </section>
   <!-- Comments -->
   <ul class="container mx-auto">
-    <li class="p-6 bg-gray-50 border border-gray-200">
+    <li
+      class="p-6 bg-gray-50 border border-gray-200"
+      v-for="comment in comments"
+      :key="comment.docID"
+    >
       <!-- Comment Author -->
       <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
+        <div class="font-bold">{{ comment.name }}</div>
+        <time>{{ comment.datePosted }}</time>
       </div>
 
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
+      <p>{{ comment.content }}</p>
     </li>
   </ul>
 </template>
@@ -100,6 +101,7 @@ export default {
       comment_show_alert: false,
       comment_alert_variant: "bg-blue-500",
       comment_alert_message: "Please wait! Your comment is being submitted.",
+      comments: [],
     };
   },
   computed: {
@@ -114,6 +116,7 @@ export default {
     }
 
     this.song = docSnapshot.data();
+    this.getComments();
   },
   methods: {
     async addComment(values, { resetForm }) {
@@ -138,6 +141,20 @@ export default {
       this.comment_alert_message = "Comment added!";
 
       resetForm();
+    },
+    async getComments() {
+      const snapshots = await commentsCollection
+        .where("sid", "==", this.$route.params.id)
+        .get();
+
+      this.comments = [];
+
+      snapshots.forEach((doc) => [
+        this.comments.push({
+          docID: doc.id,
+          ...doc.data(),
+        }),
+      ]);
     },
   },
 };
