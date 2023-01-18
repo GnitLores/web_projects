e4d2138af3cb5bf4c852c55732c195c372ccec98
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 13:53:46 +0100

    fade transitions

--- vue_practice/music/src/App.vue
+++ vue_practice/music/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <app-header />
 
-  <router-view />
+  <router-view v-slot="{ Component }"
+    ><transition name="fade" mode="out-in">
+      <component :is="Component"></component> </transition
+  ></router-view>
 
   <app-player />
 
@@ -29,3 +32,18 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
