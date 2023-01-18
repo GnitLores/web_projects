3438b87b3d4b95493ade2cc93c317f9611a78486
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-18 12:59:01 +0100

    seek and duration

--- vue_practice/music/src/stores/player.js
+++ vue_practice/music/src/stores/player.js
@@ -5,6 +5,8 @@ export default defineStore("player", {
   state: () => ({
     current_song: {},
     sound: {},
+    seek: "00:00",
+    duration: "00:00",
   }),
   actions: {
     async newSong(song) {
@@ -16,6 +18,10 @@ export default defineStore("player", {
       });
 
       this.sound.play();
+
+      this.sound.on("play", () => {
+        requestAnimationFrame(this.progress);
+      });
     },
     async toggleAudio() {
       if (!this.sound.playing) {
@@ -24,6 +30,14 @@ export default defineStore("player", {
 
       if (this.sound.playing()) {
         this.sound.pause();
+      }
+    },
+    progress() {
+      this.seek = this.sound.seek();
+      this.duration = this.sound.duration();
+
+      if (this.sound.playing()) {
+        requestAnimationFrame(this.progress);
       }
     },
   },
