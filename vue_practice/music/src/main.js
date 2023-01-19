81b427da287bb4d88f422936f0d052c531726c76
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-19 09:20:40 +0100

    internationalization library

--- vue_practice/music/src/main.js
+++ vue_practice/music/src/main.js
@@ -6,6 +6,7 @@ import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
+import i18n from "./includes/i18n";
 
 import "./assets/base.css";
 import "./assets/main.css";
@@ -19,6 +20,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia());
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.use(i18n);
     app.directive("icon", Icon);
 
     app.mount("#app");
