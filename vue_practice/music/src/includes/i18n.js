1af3609f4975da1f48b040099648d5d8d824bc40
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-19 09:28:18 +0100

    translate text

--- vue_practice/music/src/includes/i18n.js
+++ vue_practice/music/src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from "vue-i18n";
 import en from "@/locales/en.json";
+import jp from "@/locales/jp.json";
 
 export default createI18n({
   locale: "en",
   fallbackLocale: "en",
   messages: {
     en,
+    jp,
   },
 });
