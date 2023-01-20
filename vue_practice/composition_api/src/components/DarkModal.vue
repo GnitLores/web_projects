0c244fad8717dcea8982e8dc1797484c11a8f50c
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-20 14:37:15 +0100

    dynamic components

---
+++ vue_practice/composition_api/src/components/DarkModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <teleport to=".modals-container">
+    <div v-if="modelValue" class="modal-dark">
+      <h1>{{ props.title }}</h1>
+      <slot></slot>
+      <button @click="$emit('update:modelValue', false)">Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: 'No title specified',
+  },
+});
+</script>
+
+<style scoped>
+.modal-dark {
+  background: #333;
+  padding: 1rem;
+  color: white;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- vue_practice/composition_api/src/views/ModalsView.vue
+++ vue_practice/composition_api/src/views/ModalsView.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="modals">
     <h1>Modals</h1>
+    <div>
+      <label>
+        Show dark Modals <input v-model="showDarkModals" type="checkbox"
+      /></label>
+    </div>
     <button @click="showModal = true">Show modal</button>
-    <modal v-model="showModal" title="My modal title (vie prop)">
+    <component
+      v-model="showModal"
+      :is="showDarkModals ? DarkModal : Modal"
+      title="My modal title (vie prop)"
+    >
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui iste
         similique perspiciatis veniam sequi tempora. Facere nesciunt natus
         accusamus soluta, vero nam cupiditate expedita minus, quibusdam sequi,
         obcaecati molestiae. Odit.
-      </p></modal
+      </p></component
     >
   </div>
 </template>
@@ -16,7 +25,9 @@
 <script setup>
 import { ref } from 'vue';
 import Modal from '@/components/Modal.vue';
+import DarkModal from '@/components/DarkModal.vue';
 
+const showDarkModals = ref(false);
 const showModal = ref(false);
 </script>
 
