c95cd44d315337f429c7525cff62afe07dc96300
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-20 13:45:50 +0100

    modal component

---
+++ vue_practice/composition_api/src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1>This is a modal</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum quo
+        debitis dolorum doloribus eius. Pariatur fuga nam, rerum possimus natus
+        consequuntur, molestias adipisci, ad voluptas in quae consequatur ea?
+        Assumenda?
+      </p>
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.modal {
+  background: beige;
+  padding: 1rem;
+  color: grey;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- vue_practice/composition_api/src/views/ModalsView.vue
+++ vue_practice/composition_api/src/views/ModalsView.vue
@@ -2,34 +2,15 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <teleport to=".modals-container">
-      <div class="modal" v-if="showModal">
-        <h1>This is a modal</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum quo
-          debitis dolorum doloribus eius. Pariatur fuga nam, rerum possimus
-          natus consequuntur, molestias adipisci, ad voluptas in quae
-          consequatur ea? Assumenda?
-        </p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <modal v-if="showModal" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
+
 const showModal = ref(false);
 </script>
 
-<style scoped>
-.modal {
-  background: beige;
-  padding: 1rem;
-  color: grey;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
+<style scoped></style>
