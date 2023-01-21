bc2b268c8aa824945fab49f5e07ba1161d8c6508
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-21 11:26:31 +0100

    style

--- vue_practice/notepad/src/App.vue
+++ vue_practice/notepad/src/App.vue
@@ -2,7 +2,9 @@
   <NavBar />
   <!-- <RouterLink to="/" class="button">Notes</RouterLink> |
   <RouterLink to="/stats" class="button">Stats</RouterLink> -->
-  <RouterView />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
--- vue_practice/notepad/src/components/layout/NavBar.vue
+++ vue_practice/notepad/src/components/layout/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="container is-max-desktop">
+    <div class="container is-max-desktop px-2">
       <div class="navbar-brand">
         <div class="navbar-item is-size-4 is-family-monospace">Notepad</div>
         <a
