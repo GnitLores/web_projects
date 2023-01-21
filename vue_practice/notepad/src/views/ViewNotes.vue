f660304793ccc5d14a6d1e58abe01b04ccc74e7a
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-21 11:39:04 +0100

    cards

--- vue_practice/notepad/src/views/ViewNotes.vue
+++ vue_practice/notepad/src/views/ViewNotes.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="notes"><h1>Notes</h1></div>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note" />
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mb-4" v-for="i in 3">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error
+          maxime, vero deleniti voluptatem molestias ad! Architecto illum amet
+          magnam, illo sed aspernatur porro enim nam adipisci, ea sint cumque
+          non.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
 </template>
 
 <script>
