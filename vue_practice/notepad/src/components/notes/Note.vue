bcd9830758cd5d7ff59604ccb74e09ca4666b89e
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-21 14:52:37 +0100

    note length

--- vue_practice/notepad/src/components/notes/Note.vue
+++ vue_practice/notepad/src/components/notes/Note.vue
@@ -4,6 +4,9 @@
       <div class="content">
         {{ note.content }}
       </div>
+      <div class="has-text-right has-text-grey-light mt-2">
+        <small>{{ characterLength }}</small>
+      </div>
     </div>
     <footer class="card-footer">
       <a href="#" class="card-footer-item">Edit</a>
@@ -13,9 +16,17 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
   note: Object,
   required: true,
+});
+
+const characterLength = computed(() => {
+  let length = props.note.content.length;
+  let description = length > 1 ? 'characters' : 'character';
+  return `${length}  ${description}`;
 });
 </script>
 
