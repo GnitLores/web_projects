88c31f5fba596f827d62bf773ee6478708d7f04f
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-21 11:24:22 +0100

    set up navbar

--- vue_practice/notepad/src/components/layout/NavBar.vue
+++ vue_practice/notepad/src/components/layout/NavBar.vue
@@ -1,62 +1,50 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          width="112"
-          height="28"
-        />
-      </a>
-
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item"> Home </a>
-
-        <a class="navbar-item"> Documentation </a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link"> More </a>
-
-          <div class="navbar-dropdown">
-            <a class="navbar-item"> About </a>
-            <a class="navbar-item"> Jobs </a>
-            <a class="navbar-item"> Contact </a>
-            <hr class="navbar-divider" />
-            <a class="navbar-item"> Report an issue </a>
-          </div>
-        </div>
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-monospace">Notepad</div>
+        <a
+          @click.prevent="showMobileNav = !showMobileNav"
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': showMobileNav }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light"> Log in </a>
-          </div>
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': showMobileNav }"
+      >
+        <div class="navbar-end">
+          <RouterLink to="/" class="navbar-item" active-class="is-active">
+            Notes
+          </RouterLink>
+          <RouterLink to="/stats" class="navbar-item"> Stats </RouterLink>
         </div>
       </div>
     </div>
   </nav>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from 'vue';
+
+const showMobileNav = ref(false);
 </script>
 
-<style scoped></style>
+<style scoped>
+@media screen and (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
