2dea916c6512e5952a2b49a06150e6681326410a
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-21 14:45:30 +0100

    note component

---
+++ vue_practice/notepad/src/components/notes/Note.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  note: Object,
+  required: true,
+});
+</script>
+
+<style scoped></style>
--- vue_practice/notepad/src/views/ViewNotes.vue
+++ vue_practice/notepad/src/views/ViewNotes.vue
@@ -24,22 +24,13 @@
       </div>
     </div>
 
-    <div class="card mb-4" v-for="note in notes" :key="note.id">
-      <div class="card-content">
-        <div class="content">
-          {{ note.content }}
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <note v-for="note in notes" :key="note.id" :note="note" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import Note from '@/components/notes/Note.vue';
 
 const newNote = ref('');
 const newNoteRef = ref(null);
