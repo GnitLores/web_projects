70d33aa4b1fa0e098db1da7821489326b1f71279
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-21 16:06:28 +0100

    edit note component

--- vue_practice/notepad/src/components/notes/Note.vue
+++ vue_practice/notepad/src/components/notes/Note.vue
@@ -9,7 +9,9 @@
       </div>
     </div>
     <footer class="card-footer">
-      <a href="#" class="card-footer-item">Edit</a>
+      <RouterLink :to="`/editNote/${note.id}`" class="card-footer-item"
+        >Edit</RouterLink
+      >
       <a
         href="#"
         class="card-footer-item"
---
+++ vue_practice/notepad/src/views/ViewEditNote.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Edit note with an ID of : {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
