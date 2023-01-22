2c47b4ba4ada5d719637b1f917ec8f2fccf8992d
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-22 15:01:58 +0100

    auth menu items

--- vue_practice/notepad/src/views/ViewAuth.vue
+++ vue_practice/notepad/src/views/ViewAuth.vue
@@ -12,23 +12,65 @@
   <div class="card auth-form">
     <div class="card-content">
       <div class="title has-text-centered">{{ formTitle }}</div>
-      <div class="content">
-        Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros.
-        Donec id elit non mi porta gravida at eget metus. Cum sociis natoque
-        penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras
-        mattis consectetur purus sit amet fermentum.
-      </div>
+
+      <form @submit.prevent="onSubmit">
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control">
+            <input
+              v-model="credentials.email"
+              class="input"
+              type="email"
+              placeholder="e.g. alexsmith@gmail.com"
+            />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Password</label>
+          <div class="control">
+            <input
+              v-model="credentials.password"
+              class="input"
+              type="password"
+              placeholder="Enter a password"
+            />
+          </div>
+        </div>
+
+        <div class="field is-grouped is-grouped-right">
+          <p class="control">
+            <button class="button is-primary">{{ formTitle }}</button>
+          </p>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
 const register = ref(false);
 const formTitle = computed(() => {
   return register.value ? 'Register' : 'Login';
 });
+
+const credentials = reactive({
+  email: '',
+  password: '',
+});
+
+const onSubmit = () => {
+  if (!credentials.email || !credentials.password) {
+    alert('Please enter email and password.');
+  } else {
+    if (register.value) {
+      console.log('register');
+    } else {
+      console.log('login');
+    }
+  }
+};
 </script>
 
 <style scoped>
