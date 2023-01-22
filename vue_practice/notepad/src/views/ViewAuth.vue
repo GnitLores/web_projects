ed352105909769c4a997b2e9408409e76afc59f9
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-22 14:51:23 +0100

    auth page

---
+++ vue_practice/notepad/src/views/ViewAuth.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tabs is-centered">
+    <ul>
+      <li :class="{ 'is-active': !register }">
+        <a @click.prevent="register = false">Login</a>
+      </li>
+      <li :class="{ 'is-active': register }">
+        <a @click.prevent="register = true">Register</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card auth-form">
+    <div class="card-content">
+      <div class="title has-text-centered">{{ formTitle }}</div>
+      <div class="content">
+        Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros.
+        Donec id elit non mi porta gravida at eget metus. Cum sociis natoque
+        penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras
+        mattis consectetur purus sit amet fermentum.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const register = ref(false);
+const formTitle = computed(() => {
+  return register.value ? 'Register' : 'Login';
+});
+</script>
+
+<style scoped>
+.auth-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+</style>
